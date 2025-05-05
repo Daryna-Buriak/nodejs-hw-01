27953b40c543755b480d77e3a28717b68bff19ee
Daryna <buriak.daryna@gmail.com>
2025-05-05 18:09:03 +0300

    Add tasks 1-3

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,6 @@
-export const PATH_DB =
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const parthURL = fileURLToPath(import.meta.url);
+const dirname = path.dirname(parthURL);
+export const PATH_DB = path.join(dirname, '..', 'db', 'db.json');
--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,14 @@
 import { PATH_DB } from '../constants/contacts.js';
+import { readFile } from 'node:fs/promises';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  try {
+    const fileContent = await readFile(PATH_DB, 'utf-8');
+    const parsedContent = JSON.parse(fileContent);
+    return parsedContent;
+  } catch (error) {
+    console.log('Error reading contacts:', error);
+  }
+};
+
+//console.log(await readContacts());
